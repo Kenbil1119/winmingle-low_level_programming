3c8733e13fb51554c7cdbf14cb2caed0e20a3d00
Usman Saheed <uskehinde@gmail.com>
2026-04-20 15:21:30 +0100

    Know your machine type specifier size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+ /**
+ * Author: Usman Saheed
+ * Program: WinMingle Community C Training
+ * Description: Prints the size of specifier
+ */
+
+int main(void)
+{
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of double: %lu byte(s)\n", sizeof(double));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+
+	return (0);
+}
