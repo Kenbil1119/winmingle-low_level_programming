d8e3357d53d5011212f5c4a7fca66ea04b76924b
Usman Saheed <uskehinde@gmail.com>
2026-04-20 15:18:13 +0100

    "printf()"

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * Author: Usman Saheed
+ * Program: WinMingle Community C Training
+ * Description: Prints "Programming is liwith proper grammar, but the outcome is a piece of art,"
+ * 			using printf()
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
