bf2724ad982d62795b3d9c9256d8f883c8ce7b4a
Usman Saheed <uskehinde@gmail.com>
2026-04-26 00:26:59 +0100

    Modify 0-positive_or_negative.c

--- 0x01-varianles_if_else_while/0-positive_or_negative.c
+++ 0x01-varianles_if_else_while/0-positive_or_negative.c
@@ -21,17 +21,14 @@ int main(void)
 	n = rand() - avg;
 	
 	if(n > 0){
-
 		printf("%d is positive\n", n);
 		return (0);
 	}
 	if (n == 0){
-
 		printf("%d is zero\n", n);
 		return (0);
 	}
 	if (n < 0){
-
 		printf("%d is negative\n", n);
 		return (0);
 	}
