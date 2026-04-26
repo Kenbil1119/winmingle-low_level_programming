147b76001a60938854cdab61ba11b7345b1f9194
Usman Saheed <uskehinde@gmail.com>
2026-04-26 22:14:12 +0100

    Correct comments style in files

--- 0x01-varianles_if_else_while/0-positive_or_negative.c
+++ 0x01-varianles_if_else_while/0-positive_or_negative.c
@@ -3,32 +3,35 @@
 #include <time.h>
 
 /**
- * Author: Usman Saheed
- * Program: WinMingle Community C Training
- *
- * Function: main
+ * main - Prints a random number
  * Description: Prints if a randomly generated number is
  *		Positive, Negative or Zero
  *
- * Return - 0
+ * Return: 0
+ *
+ * Author: Usman Saheed
+ * Program: WinMingle Community C Training
  */
 
 int main(void)
 {
-	int n = 0, avg = RAND_MAX/2;
-	
+	int n = 0, avg = RAND_MAX / 2;
+
 	srand(time(0));
 	n = rand() - avg;
-	
-	if(n > 0){
+
+	if (n > 0)
+	{
 		printf("%d is positive\n", n);
 		return (0);
 	}
-	if (n == 0){
+	if (n == 0)
+	{
 		printf("%d is zero\n", n);
 		return (0);
 	}
-	if (n < 0){
+	if (n < 0)
+	{
 		printf("%d is negative\n", n);
 		return (0);
 	}
--- 0x01-varianles_if_else_while/1-last_digit.c
+++ 0x01-varianles_if_else_while/1-last_digit.c
@@ -3,26 +3,26 @@
 #include <time.h>
 
 /**
- * Author: Usman Saheed
- * Program: WinMingle Community C Training
- *
- * Function: main
+ * main - Print the last digit of a number.
  * Description: Print and Check if the last digit of a randomly generated
  *	number is greater than five (n > 5), equal zero (n == 0) or equal not
  *	zero but less than six (0 < n < 6)
  *
- * Return - 0
- */ 
+ * Return: 0
+ *
+ * Author: Usman Saheed
+ * Program: WinMingle Community C Training
+ */
 
 int main(void)
 {
-	int n = 0, avg = RAND_MAX/2, LastDigit = 0;
-	
+	int n = 0, avg = RAND_MAX / 2, LastDigit = 0;
+
 	srand(time(0));
 	n = rand() - avg;
 
 	LastDigit = n % 10;
-	
+
 	if (LastDigit > 5)
 		printf("Last digit of %d is %d and is Greater than 5\n", n, LastDigit);
 	else if (LastDigit == 0)
