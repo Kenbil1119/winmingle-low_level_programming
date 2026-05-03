af4971265ab89af7ecc238206516ee5fde185917
Usman Saheed <uskehinde@gmail.com>
2026-05-03 19:37:05 +0100

    Add comment to _putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,6 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _putchar - Print Character
+ * Description: Print character to the stdout using the putc()
+ *
+ * Return: 0
+ *
+ * Author: Usman Saheed
+ * Program: WinMingle Community C Training
+ */
+
 int _putchar(int c)
 {
 	if (c <= 0)
