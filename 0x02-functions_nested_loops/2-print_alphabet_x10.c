e74db4bf50bdc67e903ba7ec5fc2cc611bd70825
Usman Saheed <uskehinde@gmail.com>
2026-05-05 08:04:21 +0100

    Update header file: Add more declaration.
    New Function(print_alphabet_x10()) to print alphabet in lowercase 10 times.

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print Alphabet
+ * Description: Print in lowercase 10 times, using only two _putchar() call
+ *
+ * Return: void
+ *
+ * Author: Usman Saheed
+ * Program: WinMingle Community C Training
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+
+	return;
+}
