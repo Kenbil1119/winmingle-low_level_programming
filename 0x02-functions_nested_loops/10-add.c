1dd8b9c51ea5d91be1396c963b27116557be855b
Usman Saheed <uskehinde@gmail.com>
2026-05-11 11:14:48 +0100

    Add Function to print the sum of two integers
    Update header file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * print_number - A recursive function
+ * Description: Function breaks down a long integer value recursively
+ *		until it reach the smallest value (n < 10)
+ *		Then print from the upper stack i.e.
+ *		From left character of integer to the right
+ * @n: Long integer type parameter to be check and print
+ *
+ * return: void
+ *
+ * Author: Usman Saheed
+ */
+
+void print_number(long n)
+{
+    if (n < 0)
+    {
+        _putchar('-');
+        n *= -1;
+    }
+
+    if (n / 10) {
+        print_number(n / 10);
+    }
+
+    _putchar((n % 10) + '0');
+}
+
+/**
+ * add - Add two integers
+ * Description: Function add two integers and
+ *		invoke print_number() function to check and print to the stdout
+ *
+ * @a: First integer parameter
+ * @b: Second Integer parameter
+ *
+ * Return: Sum of a and b
+ *
+ * Author: Usman Saheed
+ * Program: WinMingle Community C Training
+ */
+
+int add(int a, int b)
+{
+    long sum = (long)a + b;
+    
+    print_number(sum);
+    _putchar('\n');
+    
+    return ((int)sum);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Test add()
+ * Description: Invoke add() function to do addition of two integer numbers
+ *
+ * Return: 0
+ *
+ * Author: Usman Saheed
+ */
+
+int main(void)
+{
+	int a = 411522630;
+
+	add(a, (a * 2));
+
+	return (0);
+}
