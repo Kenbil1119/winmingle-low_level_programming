e8e908a5ab5634d72e3fb6eb38ef7fb57924421f
Usman Saheed <uskehinde@gmail.com>
2026-05-13 14:36:29 +0100

    Add Function to print the times tables of any number in the range: 0-15

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,97 @@
+#include "main.h"
+
+void separator(void);
+long highest_place_value(long);
+
+/**
+ * print_times_table - Print multiplication table
+ * Description: Function print multiplications table
+ *		from 'n'.
+ *
+ * @n: Integer parameter for the number.
+ *
+ * Return: Nothing
+ *
+ * Author: Usman Saheed
+ * Program: WinMingle Community C Training
+ */
+
+void print_times_table(int n)
+{
+	int x, y, xy;
+	
+	if (n > 15)
+		return;
+
+	for (x = 0; x <= n; x++)
+	{
+		for (y = 0; y <= n; y++)
+		{
+			xy = x * y;
+			
+			if (y != 0 && xy < 10)
+				_putchar(' ');
+			if (y != 0 && xy < 100)
+				_putchar(' ');
+			print_number((long)xy);
+			if (y != n)
+				separator();
+		}
+		_putchar('\n');
+	}
+
+	return;
+}
+
+void print_number(long n)
+{
+	long rem = n, div = highest_place_value(n);
+
+	if (!(n < 10))
+		while (div >= 1)
+		{
+			_putchar((rem / div) + '0');
+			rem %= div;
+			div /= 10;
+		}
+	else
+		_putchar(n + '0');
+
+	return;
+}
+
+/**
+ * separator - Print comma (,) and space (  )
+ *
+ * Return: Void
+ */
+
+void separator(void)
+{
+	_putchar(',');
+	_putchar(' ');
+
+	return;
+}
+
+/**
+ * highest_place_value - Check the value of a number
+ *
+ * @num: Long parameter of a number to be check
+ *
+ * Return: Long type value of a number e.g.
+ *		456 returns 100 (Hundred).
+ */
+
+long highest_place_value(long num)
+{
+	long highest = 1;
+	
+	while (num >= 10)
+	{
+		num /= 10;
+		highest *= 10;
+	}
+	
+	return (highest);
+}
