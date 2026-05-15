2b264e7c69fcb3f1679c8b6d96e436253a07390f
Usman Saheed <uskehinde@gmail.com>
2026-05-15 15:32:07 +0100

    Print 98 Fibonacci number

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,98 @@
+#include "main.h"
+
+void separator(void);
+long highest_place_value(long);
+
+/**
+ * main - Print Fibonacci numbers
+ * Description: Function prints Fibonacci numbers under 50
+ *
+ * Return: Nothing
+ *
+ * Author: Usman Saheed
+ * Program: WinMingle Community C Training
+ */
+
+int main(void)
+{
+	long o_term = 0, n_term = 1, f_term = o_term + n_term, term = 0;
+	
+	while (term <= 98)
+	{
+		print_number(f_term);
+		if (term < 98)
+			separator();
+		else
+			_putchar('\n');
+
+		_putchar('\n');
+
+		o_term = n_term;
+		n_term = f_term;
+		f_term = o_term + n_term;
+		
+		term++;
+	}
+
+	return (0);
+}
+
+/**
+ * print_number - Print numbers by character using _putchar()
+ * @n: Long parameter for the number
+ *
+ * Return: Nothing
+ */
+
+void print_number(long n)
+{
+	long rem = n, div = highest_place_value(n);
+
+	if (!(n < 10))
+		while (div >= 1)
+		{
+			_putchar((rem / div) + '0');
+			rem %= div;
+			div /= 10;
+		}
+	else
+		_putchar(n + '0');
+
+	return;
+}
+
+/**
+ * separator - Print comma (,) and space (  )
+ *
+ * Return: Void
+ */
+
+void separator(void)
+{
+	_putchar(',');
+	_putchar(' ');
+
+	return;
+}
+
+/**
+ * highest_place_value - Check the value of a number
+ *
+ * @num: Long parameter of a number to be check
+ *
+ * Return: Long type value of a number e.g.
+ *		456 returns 100 (Hundred).
+ */
+
+long highest_place_value(long num)
+{
+	long highest = 1;
+	
+	while (num >= 10)
+	{
+		num /= 10;
+		highest *= 10;
+	}
+	
+	return (highest);
+}
