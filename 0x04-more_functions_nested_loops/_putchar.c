7a9d3f7e090ea2692195dcba63c898341af91891
Usman Saheed <uskehinde@gmail.com>
2026-05-16 07:55:58 +0100

    Header file
    Add _putchar()

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - Print Character
+ * Description: Print character to the stdout using the putc()
+ * @c: Character parameter.
+ *
+ * Return: 1 if success
+ *
+ * Author: Usman Saheed
+ * Program: WinMingle Community C Training
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
