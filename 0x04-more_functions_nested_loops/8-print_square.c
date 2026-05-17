44d5f45e88151da0464f449cc423c5b6bee9df5e
Usman Saheed <uskehinde@gmail.com>
2026-05-17 11:01:32 +0100

    Print square with '#'

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - Print square with '#'
+ *
+ * @size: Integer parameter for the size of square
+ *
+ * Return: Nothing
+ *
+ * Author: Usman Saheed
+ * Program: WinMingle Community C Training
+ */
+
+void print_square(int size)
+{
+	int length, height = size;
+
+	while (height >= 0)
+	{
+		length = size;
+		while (length)
+		{
+			_putchar('#');
+			length--;
+		}
+		_putchar('\n');
+		height--;
+	}
+
+	return;
+}
