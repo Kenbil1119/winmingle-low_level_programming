4a35638001851d6653a471bb32de27d58ef85786
Usman Saheed <uskehinde@gmail.com>
2026-05-17 08:21:32 +0100

    Print numbers from 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers from 0 to 9
+ * Description: Print all single digit numbers using only two _putchar() call
+ *
+ * Author: Usman Saheed
+ * Program: WinMingle Community C Training
+ */
+
+void print_numbers(void)
+{
+        int num;
+
+        for (num = 0; num < 10; num++)
+                putchar(num + '0');
+        putchar('\n');
+}
