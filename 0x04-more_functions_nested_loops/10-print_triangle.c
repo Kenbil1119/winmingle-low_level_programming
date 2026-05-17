9f84de149245b7a11aec0d9651278b0685f5b834
Usman Saheed <uskehinde@gmail.com>
2026-05-17 19:34:15 +0100

    Print Triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_triangle - Print Right angle triangle with '#' on call
+ *
+ * @size: Integer parameter for the size of triangle
+ *
+ * Return: EOF after execution
+ *
+ * Author: Usman Saheed
+ * Program: WinMingle Community C Training
+ */
+
+void print_triangle(int size)
+{
+	int b, h = size, hash = 0;
+
+	if (size < 1)
+	{
+		_putchar('\n');
+		return;
+	}
+	
+	while (size)
+	{
+		b = size;
+		while (b - 1)
+		{
+			_putchar(' ');
+			b--;
+		}
+
+		hash = h - (size - 1);
+		while (hash)
+		{
+			_putchar('#');
+			hash--;
+		}
+		_putchar('\n');
+		size--;
+	}
+	return;
+}
