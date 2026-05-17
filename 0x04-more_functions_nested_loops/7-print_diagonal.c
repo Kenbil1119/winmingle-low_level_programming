9bc0f2d0eb803ea9cfc1ca5c36c20e6d77509cdb
Usman Saheed <uskehinde@gmail.com>
2026-05-17 10:36:34 +0100

    Print diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Print diagonal line
+ *
+ * @n: Integer parameter for length of line
+ *
+ * Return: Void
+ *
+ * Author: Usman Saheed
+ * Program: WinMingle Community C Training
+ */
+
+void print_diagonal(int n)
+{
+	int i = 0, j = 0;
+
+	if (n < 1)
+		return;
+
+	while (i < n)
+	{
+		j = i;
+
+		while (j)
+		{
+			_putchar(' ');
+			j--;
+		}
+		_putchar('\\');
+		_putchar('\n');
+
+		i++;
+		
+	}
+}
