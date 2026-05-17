257f827f995e7fe6e9962b9dc99ca05a66dcec8f
Usman Saheed <uskehinde@gmail.com>
2026-05-17 08:32:27 +0100

    Print numbers from 0 - 9 except 2 & 4
    Modified header file
    
    Modified 3-print_numbers()

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -13,6 +13,6 @@ void print_numbers(void)
         int num;
 
         for (num = 0; num < 10; num++)
-                putchar(num + '0');
-        putchar('\n');
+		_putchar(num + '0');
+	_putchar('\n');
 }
