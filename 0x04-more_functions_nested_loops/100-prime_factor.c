80bd4d78e1e1c8ab1d15432fe57acad10e541642
Usman Saheed <uskehinde@gmail.com>
2026-05-20 13:20:31 +0100

    Highest Prime Factor of 612852475143

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,58 +1,54 @@
 #include "main.h"
 
-int is_prime(long n);
 long highest_prime_factor(long num);
 
 /**
- * highest_prime_factor - Check for the highest prime factor of a number
- *
+ * highest_prime_factor - Return the highest prime factor of a number
  * @num: Long parameter for the number
  *
- * Return: The HPF (Highest Prime Factor)
+ * Return: The highest prime factor of num
  *
  * Author: Usman Saheed
  * Program: WinMingle Community C Training
  */
-
 long highest_prime_factor(long num)
 {
-	long p_div = 2, prime = num;
+	long p_div = 2, prime = num, max_factor = 1;
 
-	for (p_div = 2; p_div < prime; p_div++)
-		if (is_prime(p_div) != 0)
-			while ((p_div < prime) && (prime % p_div) == 0)
-				prime /= p_div;
+	while (prime % p_div == 0)
+	{
+		max_factor = p_div;
+		prime /= p_div;
+	}
 
-	return (prime);
-}
+	p_div = 3;
+	while (p_div * p_div <= prime)
+	{
+		while (prime % p_div == 0)
+		{
+			max_factor = p_div;
+			prime /= p_div;
+		}
+		p_div += 2;
+	}
 
-int is_prime(long n)
-{
-	int i;
+	if (prime > 1)
+		max_factor = prime;
 
-	if (n < 2)
-		return (0);
-
-	for (i = 2; i <= n; i++)
-		if ((i != n) && (n % i == 0))
-			return (0);
-	return (1);
+	return (max_factor);
 }
 
 /**
- * main - Print HPF
- * Description: Call highest_prime_factor() to compute th
- *		highest prime factor of a number and print it.
+ * main - Print the highest prime factor for a fixed number
  *
  * Return: 0
  */
-
 int main(void)
 {
 	long num = 612852475143;
 	long HPF = highest_prime_factor(num);
 
-	printf("HPF of %ld is: %ld\n", num, HPF);
-	
+	printf("%ld\n", HPF);
+
 	return (0);
 }
