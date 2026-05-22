f33cf1b58714ceb9b470e23905f15204f8a9b7f1
Usman Saheed <uskehinde@gmail.com>
2026-05-22 01:20:42 +0100

    _putchar()

---
+++ pointers_arrays_strings/_putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - Print Character
+ * Description: Print character to the stdout using the putc()
+ *
+ * Return: 0
+ *
+ * Author: Usman Saheed
+ * Program: WinMingle Community C Training
+ */
+
+int _putchar(int c)
+{
+	return (write(1, &c, 1));
+}
