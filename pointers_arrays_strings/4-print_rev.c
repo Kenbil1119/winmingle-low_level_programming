c6b8a99198485e993cff3761015d4b4d53bfb68d
Usman Saheed <uskehinde@gmail.com>
2026-05-22 21:49:33 +0100

    Print string in Reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - Print the reverse of a string
+ *
+ * @s: Pointer to the string
+ *
+ * Author: Usman Saheed
+ * Program: WimMingle Community C Training
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	long length = 0;
+
+	if (s == NULL)
+		return;
+
+	for (i = 0; s[i] != '\0'; i++)
+		length++;
+	
+	for (i = length; i >= 0; i--)
+		_putchar(s[i]);
+}
