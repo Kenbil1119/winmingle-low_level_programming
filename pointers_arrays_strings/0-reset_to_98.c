7133c30eb602a7c80c9702cb2e7d54a489465207
Usman Saheed <uskehinde@gmail.com>
2026-05-22 01:26:52 +0100

    Reset the value of a varaible using pointer

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Reset value
+ * Description: Function reset the value of an integer variable to 98
+ *
+ * @n: Integer Pointer parameter for the address of the variable
+ *
+ * Author: Usman Saheed
+ * Program: WinMingle Community C Training
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
