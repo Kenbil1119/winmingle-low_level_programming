e9314678890c9b80618d590f520a73a879e14b99
Usman Saheed <uskehinde@gmail.com>
2026-05-22 23:24:43 +0100

    Reverse a writable string buffer

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverse a mutable string
+ *
+ * @s: Pointer to a writable string address
+ *
+ * Author: Usman Saheed
+ * Program: WimMingle Community C Training
+ */
+
+void rev_string(char *s)
+{
+	long i = 0, j, length = 0;
+	char s_rev[1024];
+
+	if (s == NULL)
+		return;
+
+	while(s[length])
+	{
+		length++;
+	}
+	
+	for (i = length - 1, j = 0; i >= 0; i--, j++)
+		s_rev[j] = s[i];
+	s_rev[j + 1] = '\0';
+	for (i = 0; i <= length; i++)
+		s[i] = s_rev[i];
+}
