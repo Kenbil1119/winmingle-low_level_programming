806ddadf14a7930ba8cf789cb346befdd766a70e
Usman Saheed <uskehinde@gmail.com>
2026-05-26 06:50:39 +0100

    Copy string from source to destination

---
+++ pointers_arrays_strings/9-stripy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcpy - Funtion copy src to dest
+ * Description: The string address pointed to by 'src' (source) is copy to the address pointed by 'dest' (destination).
+ *		'dest' should point to a valid writable address
+ *		with sufficient space to hold source.
+ *
+ * @dest: Pointer to the destination
+ * @src: Pointer to the source
+ * 
+ * Return: 'dest' after the copy
+ *
+ * Author: Usman Saheed
+ * Program: WinMingle Community C Training
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	long i = 0;
+
+	if (src == NULL)
+		return (dest);
+
+	for (i = 0; src[i] | dest[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+
+	return (dest);
+}
