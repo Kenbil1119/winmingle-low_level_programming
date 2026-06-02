e0cdb59bb67af470cc60cf8b1d17b856e7fb6b6c
Usman Saheed <uskehinde@gmail.com>
2026-06-02 07:42:55 +0100

    String Concatenation of n bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strncat - Append 'n' byte(s) from source string to destination string
+ *
+ * @dest: Pointer to writable destination
+ * @src: Pointer to the source to be concatenated
+ * @n: Number of byte(s) to copy
+ *
+ * Return: Pointer to the modified destination
+ *
+ * Author: Usman Saheed
+ * Program: WinMingle Community C Training
+ */
+
+char* _strncat(char *dest, char *src, int n)
+{
+	int i, j, length_dest;
+
+	if (src == NULL && dest != NULL)
+		return (dest);
+	if (src != NULL && dest == NULL)
+		return (src);
+	
+	if (src == NULL && dest == NULL)
+		return (NULL);
+
+	for (length_dest = 0; dest[length_dest]; length_dest++);
+
+	for (i = 0, j = length_dest; i < n; i++, j++)
+		dest[j] = src[i];
+	dest[j] = '\0';
+
+	return (dest);
+}
