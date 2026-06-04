335784924615ff49a24213442375f13982e5d027
Usman Saheed <uskehinde@gmail.com>
2026-06-04 07:28:19 +0100

    String copy n bytes

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,11 +3,11 @@
 /**
  * _strncpy - Funtion copy 'n' byte(s) of src to dest
  *
- * @dest: Pointer to the destination
+ * @dest: Pointer to the destination with sufficient and writable space
  * @src: Pointer to the source
  * @n: the size of bytes to copy (Number of elements)
  * 
- * Return: 'dest' after the copy
+ * Return: pointer to 'dest' after the copy
  *
  * Author: Usman Saheed
  * Program: WinMingle Community C Training
@@ -15,7 +15,9 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	long i = 0;
+	long i = 0, l_src;
+	
+	for (l_src = 1; src[l_src]; l_src++);
 
 	if (src == NULL && dest != NULL)
                 return (dest);
@@ -28,7 +30,8 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; i < n; i++)
 		dest[i] = src[i];
-	dest[i] = '\0';
+	if (n > l_src)
+		dest[i] = '\0';
 
 	return (dest);
 }
