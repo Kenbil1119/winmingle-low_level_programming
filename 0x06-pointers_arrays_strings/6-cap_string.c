12c2929a386dfa61b0c6cc17a5cf91d02465f566
Usman Saheed <uskehinde@gmail.com>
2026-06-07 11:08:06 +0100

    Capitalize first letter of every word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalize first letter of every word in the string pass
+ *
+ * @str: Pointer to the writable  string
+ *
+ * Return: pointer back to the string after modification
+ *
+ * Author: Usman Saheed
+ * Program: WinMingle Community C Training
+ */
+
+char *cap_string(char *str)
+{
+	unsigned i;
+	int j;
+	char break_word[3] = {' ', '\n', '\t'};
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		for (j = 0; j < 3; j++)
+			if (str[i] == break_word[j])
+				if (str[i + 1] >= '@' && str[i + 1] <= 'z')
+					str[i + 1] -= ('a' - 'A');
+
+	return (str);
+}
