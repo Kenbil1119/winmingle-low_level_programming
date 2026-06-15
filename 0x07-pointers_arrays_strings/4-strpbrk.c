23dc15aae4d47492b2a895991b680a9e638271b1
Usman Saheed <uskehinde@gmail.com>
2026-06-15 23:18:09 +0100

    Check the first occurence of buffer in accept

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Check for the first encounter byte of 'accept'
+ *		in byte of the buffer pointed to by 's'.
+ *
+ * @s: Pointer to the buffer
+ * @accept: Pointer to the specified bytes to check
+ *
+ * Return: Buffer from the first byte encounter. 0, if buffer is empty
+ *
+ * Author: Usman Saheed
+ * Program: WinMingle Community C Training
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned i;
+	int match;
+
+	if (accept == NULL || s == NULL)
+		return (0);
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			match = *s == accept[i];
+			if (match)
+				break;
+		}
+		if (match)
+			break;
+		else
+			s += 1;
+	}
+
+	return (s);
+}
