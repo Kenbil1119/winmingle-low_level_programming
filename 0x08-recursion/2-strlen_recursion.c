2d3675fecef9d5165b5ed9706151afd29b816425
Usman Saheed <uskehinde@gmail.com>
2026-06-19 15:37:59 +0100

    String length using recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - Read and Return the length of a string
+ * Description: Excluding the terminator character '\0'
+ *
+ * @s: Pointer to the string address
+ *
+ * Return: The length of the string
+ *
+ * Author: Usman Saheed
+ * Program: WinMingle Community C Training
+ */
+
+int _strlen_recursion(char *s)
+{
+	int length = 1;
+	if (s == NULL || *s == '\0')
+		return (0);
+
+	return (length + _strlen_recursion(s + length));
+}
