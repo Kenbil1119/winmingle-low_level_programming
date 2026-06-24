299016f01e0318bf7032eae4d944743b37c3c74d
Usman Saheed <uskehinde@gmail.com>
2026-06-24 20:59:08 +0100

    Factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * factorial - Compute the factorial of a number
+ *
+ * @n: The number
+ *
+ * Return: The factorial of the number, else 1 if the numberis zero.
+ *		-1 if the number is less than zero.
+ *
+ * Author: Usman Saheed
+ * Program: WinMingle Community C Training
+ */
+
+int factorial(int n)
+{
+	if (n > 0)
+		return(n * factorial(n - 1));
+	else
+		if (n == 0)
+			return (1);
+		else
+			return (-1);
+}
